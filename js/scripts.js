e968d6799d54a55e216309989e870698fe0f07ac
Samuel-Mwaura <mr.s.n.mwaura@gmail.com>
2020-02-16 11:06:27 +0300

    css edit

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+function akan() {
+var cc=
+var yy=
+var mm=
+var dd=
+var gender=
+
+var date=(((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7
+
+if (date===0 && gender===male)  {
+  alert ("")
+}
+else if (date===1 && gender===male)  {
+  alert ("")
+}
+else if (date===2 && gender===male)  {
+  alert ("")
+}
+else if (date===3 && gender===male)  {
+  alert ("")
+}
+else if (date===4 && gender===male)  {
+  alert ("")
+}
+else if (date===5 && gender===male)  {
+  alert ("")
+}
+else if (date===6 && gender===male)  {
+  alert ("")
+}
+else if (date===0 && gender===female)  {
+  alert ("")
+}
+else if (date===1 && gender===female)  {
+  alert ("")
+}
+else if (date===2 && gender===female)  {
+  alert ("")
+}
+else if (date===3 && gender===female)  {
+  alert ("")
+}
+else if (date===4 && gender===female)  {
+  alert ("")
+}
+else if (date===5 && gender===female)  {
+  alert ("")
+}
+else if (date===6 && gender===female)  {
+  alert ("")
+}
+
+}
