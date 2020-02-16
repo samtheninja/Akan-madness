8446318dea78d765f19ef2f592207ed2e1c0b75a
Samuel-Mwaura <mr.s.n.mwaura@gmail.com>
2020-02-16 17:55:41 +0300

    js

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,48 @@
 function akan() {
-var cc=
-var yy=
-var mm=
-var dd=
-var gender=
-
-var date=(((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7
-
-if (date===0 && gender===male)  {
-  alert ("")
-}
-else if (date===1 && gender===male)  {
-  alert ("")
-}
-else if (date===2 && gender===male)  {
-  alert ("")
-}
-else if (date===3 && gender===male)  {
-  alert ("")
-}
-else if (date===4 && gender===male)  {
-  alert ("")
-}
-else if (date===5 && gender===male)  {
-  alert ("")
-}
-else if (date===6 && gender===male)  {
-  alert ("")
-}
-else if (date===0 && gender===female)  {
-  alert ("")
-}
-else if (date===1 && gender===female)  {
-  alert ("")
-}
-else if (date===2 && gender===female)  {
-  alert ("")
-}
-else if (date===3 && gender===female)  {
-  alert ("")
-}
-else if (date===4 && gender===female)  {
-  alert ("")
-}
-else if (date===5 && gender===female)  {
-  alert ("")
-}
-else if (date===6 && gender===female)  {
-  alert ("")
-}
-
-}
+  var cc=parseInt(prompt("cc"))
+  var yy=parseInt(prompt("yy"))
+  var mm=parseInt(prompt("mm"))
+  var dd=parseInt(prompt("dd"))
+  
+  var gender=prompt("male/female")
+  
+  
+  var maleNames=[
+    "kwasi",
+    "kwadwo",
+    "kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ]
+  
+  var femaleNames=[
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+  ]
+  var number=(((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7
+  
+  
+  if (cc<0 || cc>20) {
+    alert("invalid century")
+  }
+  else if (yy<0 || yy>99) {
+    alert("invalid year")
+  }
+  else if (mm<=0 || mm>20) {
+    alert("invalid month")
+  }
+  else if (dd<=0 || dd>31) {
+    alert("invalid day")
+  }
+  else if (gender!=="male" && gender!=="female") {
+  alert("please enter gender")
+  }
+  }
+  akan();
